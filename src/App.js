46c01391737b6a71197a3b15242c7c63afa7761a
asxi3011 <asxi3011@gmail.com>
2022-03-20 02:50:02 +0700

    Uphost

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import PageCart from './components/PageCart'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-          <h1>Đây là trang web người dùng nhóm 9</h1>
+         <PageCart/>
+        
     </div>
   );
 }
